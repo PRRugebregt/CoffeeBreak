f30047da0d85dd3857cbe9a16c831c816ca2d04d
Patrick Rugebregt <p.r.rugebregt@gmail.com>
2022-01-06 14:19:45 +0100

    Initial Commit

---
+++ CoffeeBreak/CoffeeBreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBreakApp.swift
+//  CoffeeBreak
+//
+//  Created by Patrick Rugebregt on 06/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeBreakUITests/CoffeeBreakUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CoffeeBreakUITestsLaunchTests.swift
+//  CoffeeBreakUITests
+//
+//  Created by Patrick Rugebregt on 06/01/2022.
+//
+
+import XCTest
+
+class CoffeeBreakUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
